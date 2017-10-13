6eddefb9fe038b47a0102d23d659ba89cf45b9a1
CHOPIN Xavier <xavier.chopin@univ-lorraine.fr>
2017-10-13 14:22:12 +0200

    Added Authentication and CSRF Middlewares

--- src/Security/Middleware/CsrfMiddleware.php
+++ src/Security/Middleware/CsrfMiddleware.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * @author  Xavier Chopin <xavier.chopin@univ-lorraine.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 namespace App\Resources\Middleware;
 
 use App\Resources\TwigExtension\CsrfExtension;
---
+++ src/Security/Middleware/GuestMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * @author  Xavier Chopin <xavier.chopin@univ-lorraine.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace App\Resources\Middleware;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class GuestMiddleware extends Middleware
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        return !isset($_SESSION['phpCAS']['user']) ? $next($request, $response) : $response->withRedirect($this->router->pathFor('wrong-entry'));
+    }
+}
