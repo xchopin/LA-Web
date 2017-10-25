851491b618727f8065864dfba0b4a28b9efec74f
CHOPIN Xavier <xavier.chopin@univ-lorraine.fr>
2017-10-25 10:48:11 +0200

    Added middleware for admin contents

--- src/Admin/Resources/routes.php
+++ src/Admin/Resources/routes.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Resources\Middleware\CountryMiddleware;
-use \Security\Resources\Middleware\PersistentCSRFToken;
+use \Security\Resources\Middleware\AdminMiddleware;
 
-$app->group('/{country:[a-z]{2}}', function () use($container) {
+$app->group('/{country:[a-z]{2}}', function () use ($container) {
 
-    $this->group('/tools', function () use($container) {
+    $this->group('/tools', function () use ($container) {
         $this->map(['GET', 'POST'], '/find-student', 'AdminController:findStudent')->setName('find-student');
-    });
+    })->add(new AdminMiddleware($container));
 
 })->add(new CountryMiddleware($container));
