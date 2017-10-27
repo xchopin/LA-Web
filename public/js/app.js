58e30b297868cfe7a5cddbed2c8c012e35f0a31f
CHOPIN Xavier <xavier.chopin@univ-lorraine.fr>
2017-10-27 16:07:59 +0200

    Made application mobile friendly

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,15 @@
 
 $(function () {
     $('.message .close').on('click', function () {
-        $(this).closest('.message').transition('fade');
-    });
+        $(this).closest('.message').transition('fade')
+    })
 
+    $('.ui.sidebar').sidebar('attach events', '.toc.item')
     $('.ui.dropdown').dropdown()
-
     $('.menu .item').tab()
+
+    $('#open-menu').click(function(){
+        $('#hamburger-menu').toggleClass('open')
+    })
+
 });
