3f820fb7a3c2394a83de7e02204d7cc6a7f62c63
xchopin <xchopin54@gmail.com>
2017-11-17 10:44:40 +0100

    Added copyright

--- src/AuthBundle/AuthBundle.php
+++ src/AuthBundle/AuthBundle.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * @author  Xavier Chopin <xavier.chopin@univ-lorraine.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AuthBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
