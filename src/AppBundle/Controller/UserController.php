955344b1a75c2fdfe3171e3efb61e72b158cc869
xchopin <xchopin54@gmail.com>
2017-11-17 10:43:46 +0100

    Added bundle

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * @author  Xavier Chopin <xavier.chopin@univ-lorraine.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Controller;
+
+use CoreBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends AbstractController
+{
+}
