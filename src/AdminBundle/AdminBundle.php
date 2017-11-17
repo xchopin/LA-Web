35c5b61e49cba6d444789e19d9c098f91352b13b
xchopin <xchopin54@gmail.com>
2017-11-17 10:43:01 +0100

    Created Admin Bundle

---
+++ src/AdminBundle/AdminBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * @author  Xavier Chopin <xavier.chopin@univ-lorraine.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdminBundle extends Bundle
+{
+}
