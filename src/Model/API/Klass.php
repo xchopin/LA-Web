f7946514f4adc561713a500013deec0239d58805
xchopin <xchopin54@gmail.com>
2018-06-28 15:24:18 +0200

    Added Model Class

---
+++ src/Model/API/Klass.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * @author  Xavier Chopin <xavier.chopin@univ-lorraine.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Model\API;
+
+
+class Klass extends ApiModel
+{
+
+    public static function find(String $id)
+    {
+        return parent::get("classes/$id");
+    }
+
+    public static function enrollments(String $id)
+    {
+        return parent::get("classes/$id/enrollments");
+    }
+
+    public static function events(String $id)
+    {
+        return parent::get("classes/$id/events/stats");
+    }
+
+    public static function eventsForUser(String $id, String $userId)
+    {
+        return parent::get("classes/$id/events/user/$userId");
+    }
+
+
+}
