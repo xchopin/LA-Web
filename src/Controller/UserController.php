e7ea8866bbcfbf7dda0f0470eb4930035b0a9dc4
xchopin <xchopin54@gmail.com>
2018-06-28 15:24:01 +0200

    Added route classes/:id/

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,8 +9,34 @@
 
 namespace App\Controller;
 
+use App\Model\API\Klass;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
 
 class UserController extends AbstractController implements AuthenticatedInterface
 {
+
+    /**
+     * Shows events for a class and user given
+     *
+     * @Route("/classes/{id}", name="class-events")
+     * @param Request $request
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function classEvents(Request $request, $id = '')
+    {
+
+        //$events = Klass::events($id);
+        //var_dump($events);
+        return $this->render('User/class.twig', [
+            'givenName' => null,
+            'classes' => null,
+            'events' => null,
+            'event_activities' => null
+        ]);
+    }
+
 }
