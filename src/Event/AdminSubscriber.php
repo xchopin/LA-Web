9a6a3ede23dee9375a62d1240fefffccbafbfce6
xchopin <xchopin54@gmail.com>
2019-01-18 15:38:55 +0100

    feat: add support of LDAP group

--- src/Event/AdminSubscriber.php
+++ src/Event/AdminSubscriber.php
@@ -3,6 +3,7 @@
 namespace App\Event;
 
 use App\Controller\AdminAuthenticatedInterface;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
@@ -10,16 +11,45 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Exception\InvalidParameterException;
 
 class AdminSubscriber implements EventSubscriberInterface
 {
+    private $mode;
     private $administrators;
+    private $container;
+    private $ldap;
+    private $baseDN;
+    const PREFIX = 'ADMIN_';
+    const GROUP_MODE = 'GROUP';
+    const USERS_MODE = 'USERS';
 
     public function __construct(ContainerInterface $container)
     {
-        $this->administrators = explode(',', env('ADMINISTRATORS'));
+        $this->ldap = ldap_connect(env('LDAP_HOST'), env('LDAP_PORT'));
+        $this->baseDN = env('LDAP_BASE_DN');
+        ldap_set_option($this->ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($this->ldap, LDAP_OPT_REFERRALS, 0);
+        $container->set('ldap', /** @scrutinizer ignore-type */ $this->ldap);
+        $container->set('ldap_basedn', /** @scrutinizer ignore-type */ $this->baseDN);
+
+        $this->container = $container;
+        $this->mode = env('ADMIN_MODE');
+
+        if ($this->mode === self::GROUP_MODE)
+            $this->administrators = env(self::PREFIX . self::GROUP_MODE);
+        else if ($this->mode === self::USERS_MODE)
+            $this->administrators = explode(',', env(self::PREFIX . self::USERS_MODE));
+        else
+            throw new InvalidParameterException("Administators mode has an undefined value, please refer to the documentation.");
     }
 
+    /**
+     * Check if the logged user is Administrator
+     * It either checks if its id is in the .env list or it does an LDAP query to check if it is in the group
+     *
+     * @param FilterControllerEvent $event
+     */
     public function onKernelController(FilterControllerEvent $event)
     {
         $controller = $event->getController();
@@ -36,8 +66,17 @@ class AdminSubscriber implements EventSubscriberInterface
         if ($controller[0] instanceof AdminAuthenticatedInterface) {
             $isAdmin = false;
             if (isset($_SESSION['phpCAS']['user'])) {
-                if (in_array($_SESSION['phpCAS']['user'], $this->administrators))
-                    $isAdmin = true;
+                $userId = $_SESSION['phpCAS']['user'];
+                if ($this->mode === self::USERS_MODE) {
+                    if (in_array($userId, $this->administrators))
+                        $isAdmin = true;
+                } else {
+                    $result =  ldap_get_entries(
+                        $this->ldap,
+                        ldap_search($this->ldap, $this->baseDN, "(&(udlGroup=$this->administrators)(uid=$userId))")
+                    );
+                    $result['count'] > 0 ? $isAdmin = true : false;
+                }
             }
             if (!$isAdmin) throw new AccessDeniedHttpException('Access forbidden.');
         }
@@ -48,7 +87,6 @@ class AdminSubscriber implements EventSubscriberInterface
         if (HttpKernel::MASTER_REQUEST != $event->getRequestType())
             return;
     }
-
 
     public static function getSubscribedEvents()
     {
