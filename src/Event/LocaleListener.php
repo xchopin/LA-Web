5f0dc3a3aa5c7b117d6a7e9197be15bc0910bbef
xchopin <xchopin54@gmail.com>
2019-02-13 15:48:30 +0100

    refractor: remove MongoDB

--- src/Event/LocaleListener.php
+++ src/Event/LocaleListener.php
@@ -9,13 +9,11 @@
 
 namespace App\Event;
 
-use Jenssegers\Mongodb\Connection;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Illuminate\Database\Capsule\Manager;
 
 
 class LocaleListener
@@ -34,36 +32,8 @@ class LocaleListener
     {
         if (HttpKernel::MASTER_REQUEST != $event->getRequestType())
             return;
-
-        $this->registerCapsule();
-
     }
 
-
-    /**
-     * Sets globally and registers the Eloquent ORM's Capsule Manager into the container.
-     */
-    private function registerCapsule()
-    {
-        $capsule = new Manager();
-        $capsule->getDatabaseManager()->extend('mongodb', function($config) {
-            return new Connection($config);
-        });
-        $capsule->addConnection([
-            'driver' => 'mongodb',
-            'host' => env('DB_HOST'),
-            'port' => intval(env('DB_PORT')),
-            'database' => env('DB_NAME'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'options' => [
-                'database' => env('DB_NAME'),
-            ]
-        ]);
-        $capsule->setAsGlobal();
-        $capsule->bootEloquent();
-        $this->container->set('capsule', $capsule);
-    }
 
     public static function getSubscribedEvents()
     {
