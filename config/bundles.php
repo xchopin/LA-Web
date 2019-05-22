0e9167f5b547ac95d77a63c656882be0a7c83da2
xchopin <xchopin54@gmail.com>
2019-05-22 10:09:51 +0200

    feat: add monolog

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     JMS\I18nRoutingBundle\JMSI18nRoutingBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
