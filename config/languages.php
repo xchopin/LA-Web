bad635113e7c0532191f3f678b72b85adf5490ab
xchopin <xchopin54@gmail.com>
2019-07-10 10:21:28 +0200

    refractor: clean code

--- config/languages.php
+++ config/languages.php
@@ -1,19 +1,16 @@
 <?php
 
-use Symfony\Component\Dotenv\Dotenv;
-
 $path = $container->getParameter('kernel.root_dir') . '/../translations/*.json';
+
 $languages = [];
 $dictionaries = [];
 
 foreach (glob($path) as $file) {
     $languageId = substr(basename($file), 0, 2);
     $content = json_decode(file_get_contents($file), GLOB_BRACE);
-
-    $dictionaries += [$languageId => $content]; // ToDo : optimize by selecting only the language used
+    $dictionaries += [$languageId => $content];
     array_push($languages, $languageId);
 }
 
 $container->setParameter('languages', $languages);
-
 $container->setParameter('dictionaries', $dictionaries);
