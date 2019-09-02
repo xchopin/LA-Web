01a5e57e1e648342432321990932837578dee1b7
xchopin <xchopin54@gmail.com>
2019-09-02 09:35:02 +0200

    feat: add more features for score dashboard

--- src/Controller/ClassController.php
+++ src/Controller/ClassController.php
@@ -10,15 +10,14 @@
 namespace App\Controller;
 
 use Exception;
+use OpenLRW\Exception\GenericException as OpenLrwException;
 use OpenLRW\Exception\NotFoundException;
 use OpenLRW\Model\Klass;
 use OpenLRW\Model\Risk;
 use OpenLRW\Model\User;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Exception\SessionUnavailableException;
 
 class ClassController extends AbstractController
 {
@@ -26,16 +25,18 @@ class ClassController extends AbstractController
     /**
      * Show events for a class and a user given.
      *
-     * @Route("/classes/{id}", name="class")
+     * @Route("/classes/{id}/{date}", name="class")
      * @param Request $request
-     * @param $id
+     * @param String $id
+     * @param String|null $date
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function class(Request $request, String $id = ''): Response
+    public function class(Request $request, String $id = '', String $date = null): Response
     {
-        $class = Klass::find($id);
 
-        if ($class === null) {
+        try {
+            $class = Klass::find($id);
+        }catch (OpenLrwException $e) {
             $this->addFlash('error', 'Class does not exist');
             return $this->redirectToRoute('home');
         }
@@ -50,75 +51,102 @@ class ClassController extends AbstractController
                 ]);
             }
 
-
             if ($class->sourcedId === $enrollment->class->sourcedId) { // Check if the user is enrolled to this class
+
                 if ($enrollment->role === 'student') {
-
-
-                    try {
-                        $risk = Risk::latestByClassAndUser('23133', 'adam125u');
-                    } catch (NotFoundException $e) {
-                        $this->addFlash('error', 'This class does not have any dashboards yet');
-                        return $this->redirectToRoute('profile');
-                    }
-
-                    // - - - Risk treatment - - -
-                    $indicators = [];
-                    $scores = [];
-                    $tempWeight = [];
-                    $weight = [];
-
-                    foreach ($risk->metadata as $key => $value) {
-                        if (strpos($key, 'global') !== 0 ) {
-                            if (stripos($key, 'Weight')) {
-                                $tempWeight[] = (float)$value;
-                            }else{
-                                if (strpos($value, '/')) {
-                                    $explode = explode('/', $value);
-                                    $value = round(($explode[0] / $explode[1]) * 100, 1);
-                                }else{
-                                    $value = round($value * 100, 1);
-                                }
-                                $indicators[$key] = $value;
-                            }
-                        } else  {
-                            $trimmed = preg_replace('/\D/', '', $key); // get int value
-                            $arr = explode('/', $value, 2); // cut the string in two parts
-                            $value = $arr[0]; // take the first part of the string
-                            $scores[$trimmed] = $value; // already a percentage
-                        }
-                    }
-
-                    $res =   2*M_PI/array_sum($tempWeight);
-                    foreach ($tempWeight as $w) {
-                        $weight[] = $w * $res;
-                    }
-
-
-                    //$events = Klass::eventsForUser($id, self::loggedUser());
-                    //if ($events !== null) {
-                    //    usort($events, static function ($a, $b) {
-                    //        return $a->eventTime < $b->eventTime;
-                    //    });
-                    //}
-
-                    return $this->render('User/Class/student_class.twig', [
-                        'class' => $class,
-                        'scores' => $scores,
-                        'indicators' => $indicators,
-                        'weight' => $weight
-                    ]);
+                    return $this->studentClass($class, $date);
                 }
 
-                // If user is teacher of this class
-                return $this->render('User/Class/professor_class.twig', [
-                    'class' => $class
-                ]);
+                // Else user is teacher of this class
+                return $this->render('User/Class/professor_class.twig', ['class' => $class]);
             }
         }
 
         $this->addFlash('error', 'You are not enrolled in this class.');
         return $this->redirectToRoute('home');
+    }
+
+
+    /**
+     * Data treatment for the student class view
+     *
+     * @param $class
+     * @param $date
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    private function studentClass(Klass $class, $date)
+    {
+            $userId = self::loggedUser();
+            $classId = $class->sourcedId;
+
+            try {
+                if ($date === null) {
+                    $risk = Risk::latestByClassAndUser($classId, $userId);
+                    $date = date('Y-m-d', strtotime($risk->dateTime));
+                } else {
+                    $risk = Risk::findByClassAndUserAndDate($classId, $userId, $date);
+                    $date = date('Y-m-d', strtotime($date));
+                }
+            } catch (Exception $e) {
+                return $this->render('User/Class/student_class.twig', [
+                    'date' => $date,
+                    'class' => $class,
+                    'scores' => null
+                ]);
+            }
+
+            // - - - Risk treatment - - -
+            $indicators = [];
+            $scores = [];
+            $tempWeight = [];
+            $weight = [];
+
+            foreach ($risk->metadata as $attribute => $value) {
+                if (strpos($attribute, 'global') !== 0 ) {
+                    if (stripos($attribute, 'Weight')) {
+                        $tempWeight[] = (float)$value;
+                    }else{
+                        if (strpos($value, '/')) {
+                            $explode = explode('/', $value);
+                            $value = round(($explode[0] / $explode[1]) * 100, 1);
+                        }else{
+                            $value = round($value * 100, 1);
+                        }
+                        $indicators[$attribute] = $value;
+                    }
+                } else  {
+                    $scores = $this->extractScoreFromMetadataAttribute($scores, $attribute, $value);
+                }
+            }
+
+
+            // Calculate the radian factor
+            $res = 2 * M_PI / array_sum($tempWeight);
+
+            foreach ($tempWeight as $w) {
+                $weight[] = $w * $res;
+            }
+
+
+            $scores = $this->userWeekScores($date, $risk,   $userId);
+
+
+            return $this->render('User/Class/student_class.twig', [
+                    'date' => $date,
+                    'class' => $class,
+                    'scores' => $scores,
+                    'indicators' => $indicators,
+                    'weight' => $weight
+            ]);
+
+
+        /**$events = Klass::eventsForUser($id, self::loggedUser());
+        if ($events !== null) {
+            usort($events, static function ($a, $b) {
+                return $a->eventTime < $b->eventTime;
+            });
+        }*/
+
     }
 
     /**
@@ -150,6 +178,72 @@ class ClassController extends AbstractController
             return new Response($e->getMessage(), 404);
         }
 
+    }
+
+    /**
+     * Get the scores of a user for the 7 last days
+     *
+     * @param $initDate
+     * @param $firstRisk
+     * @param $userId
+     * @return array
+     */
+    private function userWeekScores(string $initDate, Risk $firstRisk, string $userId): array
+    {
+        $weekScores[$initDate] = $this->extractScoresFromRisk($firstRisk);
+
+        for ($i = 1; $i < 8; $i++) {
+            $day = date('Y-m-d', strtotime("-$i day", strtotime($initDate)));
+
+            try {
+                $risk = Risk::findByClassAndUserAndDate('23133', $userId, $day);
+                $dayScores = $this->extractScoresFromRisk($risk);
+            } catch (NotFoundException $e) {
+                $dayScores = null;
+            }
+
+            $weekScores[$day] = $dayScores;
+        }
+
+        return $weekScores;
+    }
+
+
+    /**
+     * Extract all the scores from the metadata attribute of a Risk object
+     *
+     * @param Risk $risk
+     * @return array
+     */
+    private function extractScoresFromRisk(Risk $risk): array
+    {
+        $scores = [];
+        foreach ($risk->metadata as $attribute => $value) {
+            $scores = $this->extractScoreFromMetadataAttribute($scores, $attribute, $value);
+        }
+
+        return $scores;
+    }
+
+
+    /**
+     * Check if the metadata attribute is a score and then place it in an array
+     *
+     * @param array $scores
+     * @param string $attribute
+     * @param $value
+     * @return array
+     */
+    private function extractScoreFromMetadataAttribute(array $scores, $attribute, $value): array
+    {
+        if (strpos($attribute, 'global') === 0 ) {
+            $trimmed = preg_replace('/\D/', '', $attribute); // get int value
+            $arr = explode('/', $value, 2); // cut the string in two parts
+            $value = $arr[0]; // take the first part of the string
+            $scores[$trimmed] = $value; // already a percentage
+        }
+
+        return $scores;
     }
 
 }
